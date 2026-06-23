6af48933ef3cc1fefd9843f883e18b3f91b4ebe2
Denis Zaharchenko <denchick1546450@gmail.com>
2026-06-24 01:25:08 +0300

    add solution

--- src/components/PeopleFilters.tsx
+++ src/components/PeopleFilters.tsx
@@ -23,7 +23,9 @@ export const PeopleFilters = () => {
     const newQuery = event.target.value;
 
     setSearchParams(
-      new URLSearchParams(getSearchWith(searchParams, { query: newQuery || null })),    
+      new URLSearchParams(
+        getSearchWith(searchParams, { query: newQuery || null }),
+      ),
     );
   };
 
@@ -67,7 +69,7 @@ export const PeopleFilters = () => {
       </div>
 
       <div className="panel-block">
-        <div className="level is-flex-grow-1 is-mobile" data-cy="CenturyFilter">        
+        <div className="level is-flex-grow-1 is-mobile" data-cy="CenturyFilter">
           <div className="level-left">
             {CENTURIES.map(century => (
               <SearchLink
